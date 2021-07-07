e58321e0a82f31aef7b5270add54a2db9a6999b0
Tom <thomas.zink@outlook.com>
2021-07-07 07:52:54 -0400

    Working through course.

--- recipeApp/src/app/recipes/recipeBook/recipe-list/recipe-list.component.ts
+++ recipeApp/src/app/recipes/recipeBook/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe'
+              ,'This is simply a test'
+              ,'https://www.recipetineats.com/wp-content/uploads/2018/09/Honey-Garlic-Chicken-Breast_5.jpg')
+    ,new Recipe('A Test Recipe 2'
+                ,'This is simply a test 2'
+                ,'https://realfood.tesco.com/media/images/RFO-1400x919-classic-chocolate-mousse-69ef9c9c-5bfb-4750-80e1-31aafbd80821-0-1400x919.jpg')
+            ];
+  
 
-  constructor() { }
+  constructor() {
+    console.log(this.recipes[0].name);
+   }
 
   ngOnInit(): void {
   }
