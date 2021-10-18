0ad8e87ea367dbef894d5d811c3996ffa2ba561d
Tom Zink <thomas.zink@outlook.com>
2021-10-18 14:55:26 -0400

    Working through the course.

--- recipeApp/src/app/recipes/shoppinglist/shoppinglist/shoppinglist.component.ts
+++ recipeApp/src/app/recipes/shoppinglist/shoppinglist/shoppinglist.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from 'src/app/Shared/ingredient.model';
+
 @Component({
   selector: 'app-shoppinglist',
   templateUrl: './shoppinglist.component.html',
   styleUrls: ['./shoppinglist.component.scss']
 })
 export class ShoppinglistComponent implements OnInit {
+  ingedients: Ingredient[] =[
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ];
 
   constructor() { }
 
